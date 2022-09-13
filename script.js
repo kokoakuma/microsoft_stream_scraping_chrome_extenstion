4e20e36b3d9868dce56a516a4cd0d75c3189e063
kokoakuma <aikw9101@gmail.com>
2022-09-13 18:03:39 +0900

    modify constructor and add docs

--- script.js
+++ script.js
@@ -8,28 +8,54 @@ function main(e){
 		return;
 	}
 
-  const videoTitleList = document.getElementsByClassName("video-title")
-  const videoViewCountList = document.getElementsByClassName("view-count")
-  const videoPublishedDateList = document.getElementsByClassName("published-date-column")
+  // get elements array
+  const videoTitleList = Array.from(document.getElementsByClassName("video-title"));
+  const videoViewCountList = Array.from(document.getElementsByClassName("view-count"));
+  const videoPublishedDateList = Array.from(document.getElementsByClassName("published-date-column"));
 
+  // create new CSV file and show download window
   const generator = new CSVGenerator(videoTitleList, videoViewCountList, videoPublishedDateList);
   const blob = generator.getCSVBlob();
   showDownloadWindow(blob);
   console.log("successfully created csv!")
 }
 
+/** @param {Blob} blob */
 function showDownloadWindow(blob){
+  const now = new Date()
+  // e.x. 2022-10-31-103020.csv
+  const nowString = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDay()}-${now.getHours()}${now.getMinutes()}${now.getSeconds()}`
   let link = document.createElement('a');
   link.href = URL.createObjectURL(blob);
-  link.download = 'test.csv';
+  link.download = `${nowString}.csv`;
   link.click();
 }
 
-// クラス
+// class definition
+/**
+ * published date class which is formatted as dd/mm/yyyy e.x. 8/9/2022
+ */
 class PublishedDate {
-  // expected mm/dd/yyyy
-  constructor(date) {
-    [this._month, this._day, this._year] = date.split("/");
+  // expects mm/dd/yyyy
+  constructor(_date) {
+    if (typeof _date != "string") {
+      throw new Error("Illegal argument was supplied! This constructor takes only string.");
+    }
+
+    const [_month, _day, _year] = _date.split("/");
+    const isMonthValid = _month.length >= 1 && _month.length <= 2
+    const isDayValid = _day.length >= 1 && _day.length <= 2
+    const isYearValid = _year.length === 4
+    if (!isMonthValid || !isDayValid || !isYearValid) {
+      throw new SyntaxError("This constructor takes only format mm/dd/yyyy.");
+    }
+
+    /** @type {string} */
+    this._month = _month;
+    /** @type {string} */
+    this._day = _day;
+    /** @type {string} */
+    this._year = _year;
   }
 
   // return yyyy/mm/dd
@@ -43,13 +69,26 @@ class PublishedDate {
   }
 }
 
+/**
+ * Stream video class
+ */
 class Video {
   static keys = ["title", "viewCount", "date"];
 
-  constructor(title, viewCount, date) {
-    this._title = title;
-    this._viewCount = viewCount;
-    this._date = new PublishedDate(date);
+  constructor(_title, _viewCount, _date) {
+    const isTitleValid = typeof _title == "string";
+    const isViewCountValid = typeof _viewCount == "string";
+    const isDateValid = _date instanceof PublishedDate;
+    if (!isTitleValid || !isViewCountValid || !isDateValid) {
+      throw new Error("Illegal argument was supplied! this constructor takes only string{title}, string{viewCount}, PublishedDate{date}.");
+    }
+
+    /** @type {string} */
+    this._title = _title;
+    /** @type {string} */
+    this._viewCount = _viewCount;
+    /** @type {PublishedDate} */
+    this._date = _date;
   }
 
   get date() {
@@ -63,22 +102,31 @@ class Video {
 
 class CSVGenerator {
   bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
-  constructor(titleList, viewCountList, dateList) {
-    this._videos = [];
-    if (titleList.length != viewCountList.length|| titleList.length != dateList.length) {
-        return;
+  constructor(_titleList, _viewCountList, _dateList) {
+    const isTitleValid = Array.isArray(_titleList);
+    const isViewCountValid = Array.isArray(_viewCountList);
+    const isDateValid = Array.isArray(_dateList);
+    if (!isTitleValid || !isViewCountValid || !isDateValid) {
+      throw new Error("Illegal argument was supplied! This constructor takes only array.");
+    }
+    if (_titleList.length != _viewCountList.length|| _titleList.length != _dateList.length) {
+      throw new Error("The length of arguments are not same.");
     }
 
-    for (var i = 0; i < titleList.length; i++) {
-      const title = titleList[i].innerText;
-      const viewCount = viewCountList[i].innerText;
-      const date = dateList[i].innerText;
+    /** @type {Array<Video>} */
+    this._videos = [];
+
+    for (var i = 0; i < _titleList.length; i++) {
+      const title = _titleList[i].innerText;
+      const viewCount = _viewCountList[i].innerText;
+      const date = new PublishedDate(_dateList[i].innerText);
       const newVideo = new Video(title, viewCount, date);
 
       this._videos.push(newVideo);
     }
   }
 
+  /** @return {Array<Video>} */
   getAsAscendingOrder() {
     return this._videos.slice().sort((prev, next) => {
       return prev.date.getDateAsMillisecond() - next.date.getDateAsMillisecond();
